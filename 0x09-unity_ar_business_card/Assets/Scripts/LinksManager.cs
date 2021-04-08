b226212fd58c0328f5c12c3dac61b518dfbcec5a
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2021-04-08 04:01:05 -0500

    Create

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/LinksManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinksManager : MonoBehaviour
+{
+    public void Medium()
+    {
+        Application.OpenURL("https://vp1312.medium.com/");
+    }
+    public void Instagram()
+    {
+        Application.OpenURL("https://www.instagram.com/victor.paz1312");
+    }
+    public void Linkedin()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/victor-manuel-paz-11b56b84/");
+    }
+    public void GitHub()
+    {
+        Application.OpenURL("https://github.com/VMP1312");
+    }
+}
