52e73ec1bc201174ebfc4aa760e512f95cd74f8b
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2021-04-19 16:55:39 -0500

    Create

---
+++ 0x0A-unity-360_video_tour/Assets/Scripts/ShowInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowInfo : MonoBehaviour
+{
+    public GameObject InfoCard;
+    private bool InfoStatus; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        InfoCard.SetActive(false);
+        InfoStatus = false; 
+    }
+    private void Show()
+    {
+        InfoCard.SetActive(true);
+        InfoStatus = true;
+    }
+    private void Hide()
+    {
+        InfoCard.SetActive(false);
+        InfoStatus = false;
+    }
+    public void Changer()
+    {
+        if(InfoStatus == false)
+            Show();
+        else
+            Hide();
+    }
+}
---
+++ 0x0A-unity-360_video_tour/Assets/Scripts/ZoneChanger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ZoneChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int scene;
+
+    public void Living()
+    {
+        animator.SetTrigger("FadeOut");
+        scene = 0;
+    }
+
+    public void Cantina()
+    {
+        animator.SetTrigger("FadeOut");
+        scene = 1;
+    }
+
+    public void Cube()
+    {
+        animator.SetTrigger("FadeOut");
+        scene = 2;
+    }
+
+    public void Mezzanine()
+    {
+        animator.SetTrigger("FadeOut");
+        scene = 3;
+    }
+
+    public void Change()
+    {
+        SceneManager.LoadScene(scene);
+        animator.SetTrigger("FadeIn");
+    }
+}
