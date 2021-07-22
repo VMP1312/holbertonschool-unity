529ba0381d80d7840929e2d6da194a0a22e7b046
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2021-07-22 15:24:50 -0500

    Upload

---
+++ 0x0E-unity-webvr/Assets/Scripts/WebTrigger.cs
@@ -0,0 +1,16 @@
+﻿using WebXR;
+using Zinnia.Action;
+using UnityEngine;
+
+public class WebTrigger : BooleanAction
+{
+
+    public WebXRController controller;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Receive(controller.GetButton(WebXRController.ButtonTypes.Grip) || controller.GetButton(WebXRController.ButtonTypes.Trigger));
+    }
+}
